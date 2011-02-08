d537bcfac1de3b1f90ad4d177b7312dba3a4bd2b
Collin Miller <collintmiller@gmail.com>
2011-02-08 14:55:49 -0500

    patch

--- lib/mail_reciever_engine/storage.rb
+++ lib/mail_reciever_engine/storage.rb
@@ -2,5 +2,5 @@ class MailRecieverEngine
   require "moneta/adapters/#{MailRecieverEngine.config.mail_reciever_engine_moneta_backend.to_s.downcase}"
   storage_class = Moneta::Adapters.const_get(MailRecieverEngine.config.mail_reciever_engine_moneta_backend)
   Storage = storage_class.new MailRecieverEngine.config.mail_reciever_engine_moneta_config
-  Storage[:all] = [] unless Storage.key??(:all)
+  Storage[:all] = [] unless Storage.key?(:all)
 end
